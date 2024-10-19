a873d7e701c86b3aba1caacac2cdfd5bc2abe4c4
mpres08 <158608642+mpres08@users.noreply.github.com>
2024-10-19 11:01:52 -0500

    passing task 6

--- src/main/java/org/launchcode/techjobs/oo/Employer.java
+++ src/main/java/org/launchcode/techjobs/oo/Employer.java
@@ -2,27 +2,10 @@ package org.launchcode.techjobs.oo;
 
 import java.util.Objects;
 
-public class Employer {
+public class Employer extends JobField{
 
-    private int id;
-    private static int nextId = 1;
-    private String value;
-
-    public Employer() {
-        id = nextId;
-        nextId++;
-    }
-
-    public Employer(String value) {
-        this();
-        this.value = value;
-    }
-
-    // Custom toString, equals, and hashCode methods:
-
-    @Override
-    public String toString() {
-        return value;
+    public Employer(String value){
+        super(value);
     }
 
     @Override
@@ -37,19 +20,4 @@ public class Employer {
     public int hashCode() {
         return Objects.hash(getId());
     }
-
-    // Getters and Setters:
-
-    public int getId() {
-        return id;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
-    }
-
 }
